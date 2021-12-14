770e252c86c49d27e0a5d0343ad4f23683d346a7
Biplob <mhemit0000@gmail.com>
2021-12-14 08:55:55 +0600

    Navbar compleate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-    <h2>Hello world</h2>
+    <Navbar />
     </div>
   );
 }
