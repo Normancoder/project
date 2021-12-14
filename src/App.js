cfddf2cc7aae57042dde4052a2c318b01cd078e3
Biplob <mhemit0000@gmail.com>
2021-12-14 10:37:17 +0600

    header compleat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import Header from './components/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
+
+
 function App() {
   return (
     <div className="App">
+      <>
+      
     <Navbar />
+    <Header />
+    </>
     </div>
   );
 }
